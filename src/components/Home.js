3e4a62570abcc72581b5c8eb62a1bbdb330f8349
OskiTheCoder <sks91@berkeley.edu>
2021-08-13 10:38:12 -0700

    update gitignore

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ import Grid from "./Grid";
 import Thumb from "./Thumb";
 import Spinner from "./Spinner";
 import SearchBar from "./SearchBar";
+import Button from "./Button";
 import { useHomeFetch } from "../hooks/useHomeFetch";
 import NoImage from "../images/no_image.jpg";
 
@@ -36,7 +37,10 @@ const Home = () => {
           />
         ))}
       </Grid>
-      <Spinner />
+      {loading && <Spinner />}
+      {state.page < state.total_pages && !loading && (
+        <Button text="Load More" />
+      )}
     </>
   );
 };
